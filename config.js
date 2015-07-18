92c1d29bbc0c6f7b6afcc2df571bb5c53e05df8d
Michael Schramek <dev@michaelschramek.de>
2015-07-18 17:15:17 +0200

    update dependencies and use browser-syc for live-reload

--- config.js
+++ config.js
@@ -15,17 +15,20 @@ System.config({
 
 System.config({
   "map": {
-    "angular": "github:angular/bower-angular@1.4.1",
-    "angular-resource": "github:angular/bower-angular-resource@1.4.0",
+    "angular": "github:angular/bower-angular@1.4.3",
+    "angular-resource": "github:angular/bower-angular-resource@1.4.3",
     "angular-ui-router": "github:angular-ui/ui-router@0.2.15",
-    "babel": "npm:babel-core@5.4.7",
-    "babel-runtime": "npm:babel-runtime@5.4.7",
+    "babel": "npm:babel-core@5.7.4",
+    "babel-runtime": "npm:babel-runtime@5.7.0",
     "core-js": "npm:core-js@0.9.13",
     "github:angular-ui/ui-router@0.2.15": {
-      "angular": "github:angular/bower-angular@1.4.1"
+      "angular": "github:angular/bower-angular@1.4.3"
     },
     "github:jspm/nodelibs-process@0.1.1": {
       "process": "npm:process@0.10.1"
+    },
+    "npm:babel-runtime@5.7.0": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
     },
     "npm:core-js@0.9.13": {
       "fs": "github:jspm/nodelibs-fs@0.1.2",
